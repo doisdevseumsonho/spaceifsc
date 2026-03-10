76e1246a40c1776ddac71a6127561ea76b4ec021
juliocesarduarte77 <julio.d12@aluno.ifsc.edu.br>
2026-03-10 14:04:48 +0000

    Fazer Tutorial do Phaser(2-3)

--- config.js
+++ config.js
@@ -1,8 +1,19 @@
 var config = {
   type: Phaser.AUTO,
-  width: 800,
-  height: 450,
+  width: 800, //Largura da tela
+  height: 450, //Altura da tela
   parent: "game-container",
+  physics: { //O Phase tem física já implementada
+    default: "arcade",
+    arcade: {
+      gravity: { y: 300 }, //1000 é o valor básico da Terra, menor faz ele pular mais, maior faz ele pular menos
+      debug: true,
+    },
+  },
+  scale: {
+    mode: Phaser.Scale.FIT, //Isso faz o jogo se adaptar a tela, sem distorcer
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+  },
 };
 
 export default config;
