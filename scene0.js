a684a9ae7f5e356bf836770a158ba11fe7621890
juliocesarduarte77 <julio.d12@aluno.ifsc.edu.br>
2026-03-17 13:54:13 +0000

    Fazer Movimento e Quase-Animação

--- scene0.js
+++ scene0.js
@@ -3,6 +3,10 @@ class scene0 extends Phaser.Scene {
 
   constructor() {
     super("scene0");
+
+    this.threshold = 0.1;
+    this.speed = 200;
+    this.direction = undefined;
   }
 
   preload() {
@@ -10,31 +14,92 @@ class scene0 extends Phaser.Scene {
       frameWidth: 64,
       frameHeight: 64,
     });
+
+    this.load.spritesheet("interact_buttom", "assets/interact_buttom.png", {
+      frameWidth: 32,
+      frameHeight: 32,
+    });
+
+    this.load.plugin(
+      "rexvirtualjoystickplugin",
+      "rexvirtualjoystickplugin.min.js",
+      true
+    );
   }
 
 
 
   create() {
     this.character = this.physics.add
-      .sprite(400, 225, "character", 20) //cria o personagem
-      .setInteractive() //permite clicar no personagem
-      .on("pointerdown", () => {
-        if (this.walking === true) {
-          this.character.play("stop-front"); //toca a animação
-          this.character.setVelocityX(0); //para o personagem
-          this.walking = false;
-        } else {
-          this.character.play("walk-right"); //toca a animação
-          this.character.setVelocityX(100); //move o personagem para a direita
-          this.walking = true;
-        }
-      });
+      .sprite(400, 225, "character", 20); //cria o personagem
 
+    //animações de andada
     this.anims.create({
       key: "walk-right",
       frames: this.anims.generateFrameNumbers("character", {
-        start: 87,
+        start: 88,
         end: 95,
+      }), //sprites são um a menos que no spritesheet.
+      frameRate: 20,
+      repeat: -1,
+    });
+
+    this.anims.create({
+      key: "walk-back",
+      frames: this.anims.generateFrameNumbers("character", {
+        start: 62,
+        end: 68,
+      }), //sprites são um a menos que no spritesheet.
+      frameRate: 20,
+      repeat: -1,
+    });
+
+    this.anims.create({
+      key: "walk-left",
+      frames: this.anims.generateFrameNumbers("character", {
+        start: 71,
+        end: 77,
+      }), //sprites são um a menos que no spritesheet.
+      frameRate: 20,
+      repeat: -1,
+    });
+
+    this.anims.create({
+      key: "walk-front",
+      frames: this.anims.generateFrameNumbers("character", {
+        start: 80,
+        end: 86,
+      }), //sprites são um a menos que no spritesheet.
+      frameRate: 20,
+      repeat: -1,
+    });
+
+    //animações de parada
+    this.anims.create({
+      key: "stop-right",
+      frames: this.anims.generateFrameNumbers("character", {
+        start: 86,
+        end: 86,
+      }), //sprites são um a menos que no spritesheet.
+      frameRate: 10,
+      repeat: -1,
+    });
+
+    this.anims.create({
+      key: "stop-back",
+      frames: this.anims.generateFrameNumbers("character", {
+        start: 60,
+        end: 60,
+      }), //sprites são um a menos que no spritesheet.
+      frameRate: 10,
+      repeat: -1,
+    });
+
+    this.anims.create({
+      key: "stop-left",
+      frames: this.anims.generateFrameNumbers("character", {
+        start: 69,
+        end: 69,
       }), //sprites são um a menos que no spritesheet.
       frameRate: 10,
       repeat: -1,
@@ -43,14 +108,70 @@ class scene0 extends Phaser.Scene {
     this.anims.create({
       key: "stop-front",
       frames: this.anims.generateFrameNumbers("character", {
-        start: 20,
-        end: 20,
+        start: 78,
+        end: 78,
       }), //sprites são um a menos que no spritesheet.
       frameRate: 10,
       repeat: -1,
     });
 
-    this.character.setCollideWorldBounds(true); //impede o personagem de sair da tela
+
+    this.joystick = this.plugins.get("rexvirtualjoystickplugin").add(this, { //puxa o plugin do joystik
+      x: 100,
+      y: 350,
+      radius: 50,
+      base: this.add.circle(0, 0, 50, 0x888888),
+      thumb: this.add.circle(0, 0, 25, 0xcccccc),
+    });
+
+    this.joystick.on("update", () => { //faz o joystick funcionar sempre que ele é "atualizado"
+      const angle = Phaser.Math.DegToRad(this.joystick.angle);
+      const force = this.joystick.force;
+      
+      if (force > this.threshold) {
+        this.direction = new Phaser.Math.Vector2(
+          Math.cos(angle),
+          Math.sin(angle),
+        ).normalize();
+      }
+
+      if (this.joystick.force > 0) {
+        this.character.setVelocity(
+          this.direction.x * this.speed,
+          this.direction.y * this.speed,
+        );
+
+        switch (true) { //checa a direção do joystick para tocar a animação correta
+          case this.joystick.angle >= -45 && this.joystick.angle < 45:
+            this.character.play("walk-right");
+            break;
+          case this.joystick.angle >= 45 && this.joystick.angle < 135:
+            this.character.play("walk-front");
+            break;
+          case this.joystick.angle < -135:
+            this.character.play("walk-left");
+            break;
+          case this.joystick.angle >= -135 && this.joystick.angle < -45:
+            this.character.play("walk-back");
+            break;
+        }
+      } else {
+        this.character.setVelocity(0, 0);
+        this.character.anims.stop();
+      }
+
+
+    });
+      this.button = this.add
+        .sprite(700, 350, "interact_buttom", 10)
+        .setInteractive()
+        .setScale(2)
+        .on("pointerdown", () => {
+          this.button.setFrame(1);
+        })
+        .on("pointerup", () => {
+          this.button.setFrame(2);
+        });
   }
 }
 
