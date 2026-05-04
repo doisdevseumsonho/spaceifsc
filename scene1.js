e73ba9b9ad3f50e2db3176bbc0115848da452e72
juliocesarduarte77 <julio.d12@aluno.ifsc.edu.br>
2026-05-04 12:12:34 +0000

    Continuar Fase Tergio

--- scene1.js
+++ scene1.js
@@ -6,18 +6,25 @@ class scene1 extends Phaser.Scene {
   }
 
   preload() {
-    this.load.spritesheet("background1", "assets/tergio_text_box.png", {
-      frameWidth: 32,
-      frameHeight: 32,
+    this.load.spritesheet("background1", "assets/characters/tergio_text_box.png", {
+      frameWidth: 320,
+      frameHeight: 230,
     });
-    this.load.image("character1", "assets/character1.png");
-    this.load.image("professor1", "assets/Tergio.png");
+    this.load.image("character1", "assets/characters/character1.png");
+    this.load.spritesheet("professor1", "assets/characters/Tergio.png", {
+      frameWidth: 32,
+      frameHeight: 64,
+    });
   }
 
   create() {
-    this.add.image(400, 300, "background1");
-    this.add.image(200, 300, "character1");
-    this.add.image(600, 300, "professor1");
+    const backgroundScaleX = 2.55; // escala horizontal da fundo
+    const backgroundScaleY = 2; // escala vertical da fundo
+    this.add.image(390, 230, "background1").
+      setScale(backgroundScaleX, backgroundScaleY);
+    this.add.image(120, 400, "character1");
+    this.add.image(770, 400, "professor1").setScale(-1, 1);
+
   }
 
   update() {
