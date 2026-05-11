9f7b3cf94438810b8cb6bb686414899c4bbee80b
juliocesarduarte77 <julio.d12@aluno.ifsc.edu.br>
2026-05-11 12:17:45 +0000

    Continuar Fase Tergio (3)

--- client/scene1.js
+++ client/scene1.js
@@ -1,11 +1,10 @@
 class scene1 extends Phaser.Scene {
-  questionNumberText = "1. ";
-  question = "15 + 13:";
+  questionNumberText = "1: ";
+  question = "15 + 13?";
   questionNumber = 1;
 
   constructor() {
     super("scene1");
-
   }
 
   preload() {
@@ -21,120 +20,176 @@ class scene1 extends Phaser.Scene {
     });
 
     //Interagíveis
-    this.load.image("selectionButton1", "assets/quiz/selection_box_question.png");
+    this.load.image(
+      "selectionButton1",
+      "assets/quiz/selection_box_question.png",
+    );
     this.load.spritesheet("button1", "assets/quiz/questionbox.png", {
       frameWidth: 192,
       frameHeight: 192,
     });
-    this.load.image("selectionButton2", "assets/quiz/selection_box_question.png");
+    this.load.image(
+      "selectionButton2",
+      "assets/quiz/selection_box_question.png",
+    );
     this.load.spritesheet("button2", "assets/quiz/questionbox.png", {
       frameWidth: 192,
       frameHeight: 192,
     });
-    this.load.image("selectionButton3", "assets/quiz/selection_box_question.png");
+    this.load.image(
+      "selectionButton3",
+      "assets/quiz/selection_box_question.png",
+    );
     this.load.spritesheet("button3", "assets/quiz/questionbox.png", {
       frameWidth: 192,
       frameHeight: 192,
     });
-    this.load.image("selectionButton4", "assets/quiz/selection_box_question.png");
+    this.load.image(
+      "selectionButton4",
+      "assets/quiz/selection_box_question.png",
+    );
     this.load.spritesheet("button4", "assets/quiz/questionbox.png", {
       frameWidth: 192,
       frameHeight: 192,
     });
-
   }
 
   create() {
     //Fundo
     const backgroundScaleX = 2.55; // escala horizontal da fundo
     const backgroundScaleY = 2; // escala vertical da fundo
-    this.add.image(390, 230, "background1").
-      setScale(backgroundScaleX, backgroundScaleY);
+    this.add
+      .image(390, 230, "background1")
+      .setScale(backgroundScaleX, backgroundScaleY);
     this.add.image(120, 400, "character1");
     this.add.image(770, 400, "professor1").setScale(-1, 1);
 
     //Interagíveis
     const selectionButtonScaleX = 6; // escala horizontal da seleção
     const selectionButtonScaleY = 3; // escala vertical da seleção
-    
+    const debugplaybuttonVisible = true;
+
+    this.add.sprite(320, 190, "button1", 0).setScale(1);
+    this.questionText1 = this.add.text(320, 190, "25", {
+      fontSize: "32px",
+      fill: "#fff",
+    });
     this.selectionButton1 = this.physics.add
-      .sprite(320, 340, "selectionButton1", 0)
+      .sprite(320, 190, "selectionButton1", 0)
       .setInteractive()
       .setScale(selectionButtonScaleX, selectionButtonScaleY)
+      .setAlpha(debugplaybuttonVisible ? 0.01 : 0) // meio transparente para facilitar o debug
+      .on("pointerdown", () => {
+        if (this.questionNumber === 1) {
+          this.wrongAnswer1();
+        } // Ação a ser executada quando o botão for clicado
+      });
+
+    this.add.sprite(560, 190, "button2", 0).setScale(1).setInteractive();
+    this.questionText2 = this.add.text(560, 190, "30", {
+      fontSize: "32px",
+      fill: "#fff",
+    });
+    this.selectionButton2 = this.physics.add
+      .sprite(560, 190, "selectionButton2", 0)
+      .setInteractive()
+      .setScale(selectionButtonScaleX, selectionButtonScaleY)
+      .setAlpha(debugplaybuttonVisible ? 0.01 : 0) // meio transparente para facilitar o debug
+      .on("pointerdown", () => {
+        if (this.questionNumber === 1) {
+          this.wrongAnswer1();
+        } // Ação a ser executada quando o botão for clicado
+      });
+
+    this.add.sprite(320, 340, "button3", 0).setScale(1);
+    this.questionText3 = this.add.text(320, 340, "28", {
+      fontSize: "32px",
+      fill: "#fff",
+    });
+    this.selectionButton3 = this.physics.add
+      .sprite(320, 340, "selectionButton3", 0)
+      .setInteractive()
+      .setScale(selectionButtonScaleX, selectionButtonScaleY)
+      .setAlpha(debugplaybuttonVisible ? 0.01 : 0) // meio transparente para facilitar o debug
       .on("pointerdown", () => {
         if (this.questionNumber === 1) {
           this.correctAnswer1();
         }
       });
-    this.add
-      .sprite(320, 340, "button1", 0)
-      .setScale(1)
-      .setInteractive()
-      .on("pointerdown", () => {
-        // Ação a ser executada quando o botão for clicado
-      });
-    
-    this.selectionButton2 = this.physics.add
-      .sprite(560, 340, "selectionButton2", 0)
-      .setInteractive()
-      .setScale(selectionButtonScaleX, selectionButtonScaleY)
-      .on("pointerdown", () => {
-        // Ação a ser executada quando o botão for clicado
-      });
-    this.add
-      .sprite(560, 340, "button2", 0)
-      .setScale(1)
-      .setInteractive()
-      .on("pointerdown", () => {
-      });
-    
-    this.selectionButton3 = this.physics.add
-      .sprite(320, 190, "selectionButton3", 0)
-      .setInteractive()
-      .setScale(selectionButtonScaleX, selectionButtonScaleY)
-      .on("pointerdown", () => {
-        // Ação a ser executada quando o botão for clicado
-      });
-    this.add
-      .sprite(320, 190, "button3", 0)
-      .setScale(1)
-      .setInteractive()
-      .on("pointerdown", () => {
-      });
-    
+
+    this.add.sprite(560, 340, "button4", 0).setScale(1);
+    this.questionText4 = this.add.text(560, 340, "26", {
+      fontSize: "32px",
+      fill: "#fff",
+    });
     this.selectionButton4 = this.physics.add
-      .sprite(560, 190, "selectionButton4", 0)
+      .sprite(560, 340, "selectionButton4", 0)
       .setInteractive()
       .setScale(selectionButtonScaleX, selectionButtonScaleY)
+      .setAlpha(debugplaybuttonVisible ? 0.01 : 0) // meio transparente para facilitar o debug
       .on("pointerdown", () => {
-        // Ação a ser executada quando o botão for clicado
-      });
-    this.add
-      .sprite(560, 190, "button4", 0)
-      .setScale(1)
-      .setInteractive()
-      .on("pointerdown", () => {
+        if (this.questionNumber === 1) {
+          this.wrongAnswer1();
+        } // Ação a ser executada quando o botão for clicado
       });
 
     //textos
-    this.questionText = this.add
-      .text(180, 30, this.questionNumberText + this.question, {
+    this.questionText = this.add.text(180, 30, this.questionNumberText + this.question, {
         fontSize: "32px",
         fill: "#fff",
       });
-  }
 
-  correctAnswer1() {
-    this.questionNumberText = "2. ";
-    this.question = "6 - 7:";
-    this.questionNumber = 2;
-    this.questionText.setText(this.questionNumberText + this.question);
+    this.pointsText = this.add
+      .text(560, 30, "Pontuação:" + this.game.points, {
+        fontSize: "32px",
+        fill: "#fff",
+      })
+      .setScrollFactor(0);
   }
 
   // Lógica de atualização da cena
-  
-
-  update() {
+  correctAnswer1() {
+    this.questionNumberText = "2: ";
+    this.question = "6 - 7?";
+    this.questionNumber = 2;
+    this.questionText.setText(this.questionNumberText + this.question);
+    this.game.points = this.game.points + 10;
+    this.pointsText.setText("Pontuação:" + this.game.points);
+    this.questionText1.setText("13");
+    this.questionText2.setText("-1");
+    this.questionText3.setText("0");
+    this.questionText4.setText("1");
   }
+  wrongAnswer1() {
+    this.questionNumberText = "2: ";
+    this.question = "6 - 7?";
+    this.questionNumber = 2;
+    this.questionText.setText(this.questionNumberText + this.question);
+    this.pointsText.setText("Pontuação:" + this.game.points);
+    this.questionText1.setText("13");
+    this.questionText2.setText("-1");
+    this.questionText3.setText("0");
+    this.questionText4.setText("1");
+  }
+
+  correctAnswer2() {
+    this.questionNumberText = "3: ";
+    this.question = "10 x 11?";
+    this.questionNumber = 3;
+    this.questionText.setText(this.questionNumberText + this.question);
+    this.game.points = this.game.points + 10;
+    this.pointsText.setText("Pontuação:" + this.game.points);
+  }
+  wrongAnswer2() {
+    this.questionNumberText = "3: ";
+    this.question = "10 x 11?";
+    this.questionNumber = 3;
+    this.questionText.setText(this.questionNumberText + this.question);
+    this.pointsText.setText("Pontuação:" + this.game.points);
+  }
+
+
+
+  update() {}
 }
 export default scene1;
