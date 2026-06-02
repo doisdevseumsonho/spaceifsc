3cd2b0755170b9eb7b0fe80bb9c0378cf8c1a43b
juliocesarduarte77 <julio.d12@aluno.ifsc.edu.br>
2026-06-02 12:25:05 +0000

    Arrumar Tutorial 1 (2)

--- client/js/scenetutorial1.js
+++ client/js/scenetutorial1.js
@@ -13,12 +13,24 @@ class scenetutorial1 extends Phaser.Scene {
     })
       .setOrigin(0.5);
 
-    this.add.text(400, 200, "Para interagir com personagens e\nalguns objetos, basta se aproximar\ne apertar o botão de interação", {
+    this.add.text(400, 200, "Para interagir com personagens e\nalguns objetos, basta se aproximar\ne apertar o botão de interação.", {
       fontSize: "24px",
       fill: "#000000",
       align: "center",
       wordWrap: { width: 700 },
     }).setOrigin(0.5);
+
+    this.add.text(400, 250, "'   '", {
+      fontSize: "24px",
+      fill: "#000000",
+      align: "center",
+      wordWrap: { width: 700 },
+    }).setOrigin(0.5);
+
+    this.add.image(400, 250, "interact_buttom").setScale(1);
+    this.time.delayedCall(10000, () => {
+      this.scene.start("scene0");
+    });
 
   }
 
