ccc5a5341f966515340f3e6e5a10369e1ab2ef03
juliocesarduarte77 <julio.d12@aluno.ifsc.edu.br>
2026-06-02 13:01:17 +0000

    Arrumar Tutorial 3 (1)

--- client/js/scenetutorial3.js
+++ client/js/scenetutorial3.js
@@ -3,9 +3,33 @@ class scenetutorial3 extends Phaser.Scene {
     super("scenetutorial3");
   }
 
-  preload() {}
+  create() {
+    this.add.image(400, 300, "backgroundtergio0");
+    this.add.image(410, 230, "backgroundtergio1").setScale(2.15, 2);
+    this.add
+      .text(400, 100, "Campo Minado:", {
+        fontSize: "32px",
+        fill: "#000000",
+      })
+      .setOrigin(0.5);
 
-  create() {}
+    this.add
+      .text(400, 200, "Clique em um espaço para atirar ali.\nEvite atirar nos planetas e revele\ntodos os espaços para vencer o jogo.\nO número em um espaço indica quantos\nplanetas estão adjacentes a ele,\nincluindo diagonais.",
+        {
+          fontSize: "24px",
+          fill: "#000000",
+          align: "center",
+          wordWrap: { width: 700 },
+        },
+      )
+      .setOrigin(0.5);
+
+      this.add.image(400, 350, "professor2").setScale(2);
+
+    this.time.delayedCall(12000, () => {
+      this.scene.start("scene2");
+    });
+  }
 
   update() {}
 }
